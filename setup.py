1a684098aed45aecc612537409c23e94679598de
Stephan McLean <stephan.mclean2@mail.dcu.ie>
2013-11-12 22:31:50 +0000

    Init

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='kat',
+      version='0.1',
+      description='An unofficial Python API for http://kickass.to/',
+      url='https://github.com/stephan-mclean/KAT',
+      author='Stephan McLean',
+      author_email='stephan.mclean2@mail.dcu.ie',
+      license='MIT',
+      packages=['kat'],)
