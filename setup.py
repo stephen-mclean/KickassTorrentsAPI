84e4b01cf7c84cbf6b510f94cd1dc6d9f3e4abe3
Stephan McLean <stephan.mclean2@mail.dcu.ie>
2013-11-12 23:39:13 +0000

    Changed to module rather than package.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 setup(name='kat',
-      version='0.1.3',
+      version='0.1.9',
       description='An unofficial Python API for http://kickass.to/',
       url='https://github.com/stephan-mclean/KAT',
       author='Stephan McLean',
       author_email='stephan.mclean2@mail.dcu.ie',
       license='MIT',
-      py_modules=['kat'],)
+      py_modules=['kat'])
